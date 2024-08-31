23b0f0da21b4630fba308f800115b2c199c146e4
mm <malek.miladi@etudiant-fst.utm.tn>
2024-08-31 17:46:52 +0100

    fix insertion sort loop condition

--- src/main/java/org/noskl/sortingAlgorithms/InsertionSort.java
+++ src/main/java/org/noskl/sortingAlgorithms/InsertionSort.java
@@ -9,12 +9,10 @@ public class InsertionSort {
     public static void sort(int[] arr) {
         int l = arr.length;
         for (int i = 0; i < l; i++) {
-            for (int j = i; j > 0; j--) {
-                if (arr[j] < arr[j - 1]) {
-                    arr[j - 1] = arr[j - 1] ^ arr[j];
-                    arr[j] = arr[j - 1] ^ arr[j];
-                    arr[j - 1] = arr[j - 1] ^ arr[j];
-                }
+            for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+                arr[j - 1] = arr[j - 1] ^ arr[j];
+                arr[j] = arr[j - 1] ^ arr[j];
+                arr[j - 1] = arr[j - 1] ^ arr[j];
             }
         }
     }
